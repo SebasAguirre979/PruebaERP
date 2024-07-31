eabb2123c181712c545bae718d0dee4560a590f2
SebasAguirre979 <sebastian.aguirre.rodriguez@correounivalle.edu.co>
2024-07-31 17:31:15 -0500

    modelo y crud basicos

--- erp/urls.py
+++ erp/urls.py
@@ -1,23 +1,18 @@
-"""
-URL configuration for erp project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from seguridad.views import *
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from seguridad.views import UserViewSet, RoleViewSet, PermissionViewSet, ModuleViewSet, RolePermissionViewSet, UserRoleViewSet, UserPermissionViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'roles', RoleViewSet)
+router.register(r'permissions', PermissionViewSet)
+router.register(r'modules', ModuleViewSet)
+router.register(r'role-permissions', RolePermissionViewSet)
+router.register(r'user-roles', UserRoleViewSet)
+router.register(r'user-permissions', UserPermissionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
--- seguridad/views.py
+++ seguridad/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Role, Permission, Module, RolePermission, UserRole, UserPermission
+from .serializers import UserSerializer, RoleSerializer, PermissionSerializer, ModuleSerializer, RolePermissionSerializer, UserRoleSerializer, UserPermissionSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class PermissionViewSet(viewsets.ModelViewSet):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+
+class ModuleViewSet(viewsets.ModelViewSet):
+    queryset = Module.objects.all()
+    serializer_class = ModuleSerializer
+
+class RolePermissionViewSet(viewsets.ModelViewSet):
+    queryset = RolePermission.objects.all()
+    serializer_class = RolePermissionSerializer
+
+class UserRoleViewSet(viewsets.ModelViewSet):
+    queryset = UserRole.objects.all()
+    serializer_class = UserRoleSerializer
+
+class UserPermissionViewSet(viewsets.ModelViewSet):
+    queryset = UserPermission.objects.all()
+    serializer_class = UserPermissionSerializer
